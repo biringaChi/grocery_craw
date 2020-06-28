68606bb392857b81a1c6299d83cd77bb6991b679
Chi <biringachidera@gmail.com>
2020-06-28 13:49:30 -0400

    Update middlewares.py

--- grocery_craw/middlewares.py
+++ grocery_craw/middlewares.py
@@ -30,7 +30,6 @@ class GroceryCrawSpiderMiddleware:
 class GroceryCrawDownloaderMiddleware:
     @classmethod
     def from_crawler(cls, crawler):
-        # This method is used by Scrapy to create your spiders.
         s = cls()
         crawler.signals.connect(s.spider_opened, signal=signals.spider_opened)
         return s
